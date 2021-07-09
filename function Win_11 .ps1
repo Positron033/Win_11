23827c616591e75894903672382087b8890d1706
Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
2021-07-09 09:13:24 +0200

    Update function Win_11 .ps1

--- function Win_11 .ps1
+++ function Win_11 .ps1
@@ -3,12 +3,13 @@ function _AchCheck {
     param (
 
     # architecture processeur       
-    $CheckArchCPU = (Get-CimInstance -ClassName CIM_Processor).AddressWidth
+    $CheckArchCPU = (Get-CimInstance -ClassName CIM_Processor).AddressWidth;
+    # architecture OS
     $CheckArchOS = (Get-CimInstance -ClassName CIM_OperatingSystem).OSArchitecture
 
 Process {
 
-    if ( $CheckArchCPU -eq "64" -and $CheckArchOS -eq "64 bits")
+    if ( ($CheckArchCPU -eq "64") -and ($CheckArchOS -eq "64 bits"))
     
     
 }
