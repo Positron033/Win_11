0a83c72ce18a4f024591be0564a743fb3360cb16
Positron033 <baneysebastien@live.fr>
2021-07-14 14:06:23 +0200

    modif mineure

--- function Win_11 .ps1
+++ function Win_11 .ps1
@@ -1133,7 +1133,7 @@ if (_CheckSecureBoot -eq $True) {
 else {
     
     Write-Host "FAIL " -ForegroundColor Red -NoNewline
-    Write-Output "Votre machine n'est pas compatible Secureboot ou vérifiez qu'il soit activer sur votre machine"
+    Write-Output "Votre machine n'est pas compatible Secureboot ou verifiez qu'il soit activer sur votre machine"
 }
 
 if (_CheckTpm -eq $True) {
